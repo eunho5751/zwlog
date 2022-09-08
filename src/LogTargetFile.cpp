f4d4e51481111e07929baf6a583524433c587a06
LAPTOP-VQJCEHJJ\eunho <eunho5751@naver.com>
2022-09-08 21:20:19 +0900

    Update LogTargetFile to create directories that do not exist

--- src/LogTargetFile.cpp
+++ src/LogTargetFile.cpp
@@ -3,14 +3,21 @@
 
 #include <chrono>
 #include <format>
+#include <filesystem>
 #include <utility>
 
 namespace zwlog
 {
 	LogTargetFile::LogTargetFile(std::string file_name, OpenMode open_mode) : open_mode_(open_mode)
 	{
-		std::ios_base::openmode mode = std::ios_base::out;
+		// Create directories if not exists
+		if (auto sep_idx = file_name.find_last_of('/'); sep_idx != std::string::npos && sep_idx != 0)
+		{
+			std::string directories = file_name.substr(0, sep_idx);
+			std::filesystem::create_directories(directories);
+		}
 
+		std::ios_base::openmode mode = std::ios_base::out;
 		switch (open_mode_)
 		{
 
@@ -20,7 +27,7 @@ namespace zwlog
 
 			auto local_time = zoned_time(current_zone(), system_clock::now()).get_local_time();
 			std::string time_string = std::format("{0:%Y%m%d%H%M}", local_time);
-
+			
 			if (auto ext_idx = file_name.find_last_of('.'); ext_idx != std::string::npos)
 			{
 				std::string name = file_name.substr(0, ext_idx);
